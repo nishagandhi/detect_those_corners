df9c47914ab702f0a793af0d84e1404a2aa4c214
nishagandhi <nishagandhi934@gmail.com>
2018-07-02 21:02:48 -0400

    Updated the code

--- harris_corners.py
+++ harris_corners.py
@@ -3,6 +3,17 @@ import cv2
 import sys,getopt
 import copy as cp
 
+def harris_corners(image):
+
+    gray_img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    gray_img = np.float32(gray_img)
+    
+    corners_img = cv2.cornerHarris(gray_img,3,3,0.04)
+
+    image[corners_img>0.001*corners_img.max()] = [0,255,0]
+
+    return image
+
 def main():
     
     cap = cv2.VideoCapture(0)
@@ -10,20 +21,9 @@ def main():
     while(True):
 
         ret,img = cap.read()
-
-        #img = cv2.imread('/home/nisha/Downloads/me.jpg')
-        img1 = cp.copy(img)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-        gray = np.float32(gray)
-        dst = cv2.cornerHarris(gray,2,3,0.04)
-
-        dst = cv2.dilate(dst,None)
-
-        img[dst>0.001*dst.max()]=[0,255,0]
-
-        cv2.imshow('Original',img1)
-        cv2.imshow('Harris',img)
+        cv2.imshow('Original Image',img)
+        corners = harris_corners(img)
+        cv2.imshow('After detecting Harris corners',corners)
         
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
